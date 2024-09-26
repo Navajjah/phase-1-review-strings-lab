72a38be4309e9e2568d8c05f7cadcfe7afeb17ba
Debrah Muinde <debbynav645@gmail.com>
2024-09-26 07:21:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+console.log(currentUser)
+
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage)
+
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+console.log(excitedWelcomeMessage)
+
+const shortGreeting=`Welcome, ${currentUser.substring(0,1)}!`;
+console.log(shortGreeting)
